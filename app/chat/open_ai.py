e50d778ae7709b8dda4da9f22c8fa918f7de1423
Wai Phyo N <waiphyo.dev@gmail.com>
2025-04-16 15:15:22 +0700

    [ Fix ] refactor main code

---
+++ app/chat/open_ai.py
@@ -0,0 +1,27 @@
+import openai
+
+from app.core.config import config
+from app.core.logger import logger
+
+openai.api_key = config.openai_key
+
+def await_chat_cli():
+    logger.info("💬 ChatGPT CLI (type 'exit' to quit)\n")
+    messages = [{"role": "system", "content": "You are a helpful assistant."}]
+
+    while True:
+        user_input = input("You: ").strip()
+        
+        if user_input.lower() == "exit":
+            print("👋 Bye!")
+            break
+
+        messages.append({"role": "user", "content": user_input})
+
+        try:
+            response =  openai.ChatCompletion.create(model="gpt-4o-mini", messages=messages)
+            reply = response.choices[0].message.content.strip()
+            print(f"🤖 GPT: {reply}")
+            messages.append({"role": "assistant", "content": reply})
+        except Exception as e:
+            print(f"👻 Error: {e}")
--- app/main.py
+++ app/main.py
@@ -1,36 +1,10 @@
-import openai
 
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-
-from app.core.config import config
-from app.core.logger import logger
 from app.middlewares.watching_dog import WatchDogMiddleware
 
-from app.api.routes_mock import router_mock
-
-openai.api_key = config.openai_key
-
-async def await_chat_cli():
-    logger.info("💬 ChatGPT CLI (type 'exit' to quit)\n")
-    messages = [{"role": "system", "content": "You are a helpful assistant."}]
-
-    while True:
-        user_input = input("You: ").strip()
-        
-        if user_input.lower() == "exit":
-            print("👋 Bye!")
-            break
-
-        messages.append({"role": "user", "content": user_input})
-
-        try:
-            response = await openai.ChatCompletion.create(model="gpt-4o-mini", messages=messages)
-            reply = response.choices[0].message.content.strip()
-            print(f"🤖 GPT: {reply}")
-            messages.append({"role": "assistant", "content": reply})
-        except Exception as e:
-            print(f"👻 Error: {e}")
+from app.api.mock_api import mock_router
+from app.chat.open_ai import await_chat_cli
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -41,8 +15,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(WatchDogMiddleware)
 
-app.include_router(router_mock, prefix="/mock")
-
+app.include_router(mock_router, prefix="/mock")
 
 if __name__ == "__main__":
     import uvicorn
